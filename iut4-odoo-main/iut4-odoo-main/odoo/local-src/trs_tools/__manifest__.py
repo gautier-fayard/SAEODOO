eed7edd94c128efeb115ec71f4e70838bc2cfa9a
Gautier Fayard <gautier.fayard@etu.univ-smb.fr>
2026-03-15 12:47:50 +0100

    Add files via upload

---
+++ iut4-odoo-main/iut4-odoo-main/odoo/local-src/trs_tools/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "TR Solutions - Tools",
+    "version": "18.0.1.0.0",
+    "author": "TR Solutions",
+    "license": "AGPL-3",
+    "category": "Others",
+    "website": "https://tr-solutions.fr",
+    "depends": [
+        "mail",
+        "fs_attachment",
+        "fs_storage",
+    ],
+    "data": [
+        "data/storage_data.xml",
+    ],
+}
